918b20b05cb04a769d4caf15a2451bcdf9a685f9
Hideki Abe <hideki.abe.0630@gmail.com>
2016-05-27 09:24:12 +0900

    Remove white space

--- plugins/BuildFileFilter/lib/BuildFileFilter/Plugin.pm
+++ plugins/BuildFileFilter/lib/BuildFileFilter/Plugin.pm
@@ -7,7 +7,7 @@ sub build_file_filter {
     if ($args{Blog}->id == 1) {
         if ($args{Entry}->authored_on + 0 < 20160101000000) {
             return 0; # Don't publish.
-        } 
+        }
     }
 
     return 1;
