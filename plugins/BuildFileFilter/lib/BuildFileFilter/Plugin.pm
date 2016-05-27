16a0f2655c670bc11ddecc50f418dd86e7f0b4eb
Hideki Abe <hideki.abe.0630@gmail.com>
2016-05-27 10:07:12 +0900

    フィルタを記事テンプレートの場合のみに適用する処理を追加

--- plugins/BuildFileFilter/lib/BuildFileFilter/Plugin.pm
+++ plugins/BuildFileFilter/lib/BuildFileFilter/Plugin.pm
@@ -2,11 +2,17 @@ package BuildFileFilter::Plugin;
 use strict;
 use CustomFields::Util qw( get_meta );
 
+# Note:
+# $args{ArchiveType}
+# ‘index’, ‘Individual’, ‘Page’, ‘Category’, ‘Daily’, ‘Monthly’, or ‘Weekly’
+
 sub build_file_filter {
     my ($cb, %args) = @_;
 
     # By Entry authored_on Data
     if ($args{Blog}->id == 1) {
+        return 1 if $args{ArchiveType} ne 'Individual';
+
         if ($args{Entry}->authored_on + 0 < 20160101000000) {
             return 0; # Don't publish.
         }
@@ -14,12 +20,12 @@ sub build_file_filter {
 
     # By MTEntryNoPublish Custom Field
     if ($args{Blog}->id == 10) {
-        if ($args{Entry}) {
-            my $meta = get_meta($args{Entry});
+        return 1 if $args{ArchiveType} ne 'Individual';
 
-            if ($meta->{ entry_no_publish }) {
-                return 0; # Don't publish.
-            }
+        my $meta = get_meta($args{Entry});
+
+        if ($meta->{ entry_no_publish }) {
+            return 0; # Don't publish.
         }
     }
 
